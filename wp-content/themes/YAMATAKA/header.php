0b58b11de8ba57aacee0fc0d9e9a490c5632e426
Nora <nsorym04@gmail.com>
2023-03-07 01:52:19 +0900

    メインヒーロー完成仮

--- wp-content/themes/YAMATAKA/header.php
+++ wp-content/themes/YAMATAKA/header.php
@@ -25,7 +25,41 @@
     <section class="<?php echo $heroClass = is_home() || is_front_page() ? 'hero' : 'sub_hero' ?>">
         <?php if (is_home() || is_front_page()) :; ?>
             <div class="hero_txt">
-                <h1>いつでもそばに<br>いつでもあなたの健康を考えます</h1>
+                <h1>いつでもそばに<br>いつでもあなたの<br>健康を考えます</h1>
+            </div>
+            <div class="hero_info flex_column">
+                <div class="hero_info_time">
+                    <p>外来受付</p>
+                    <div class="hero_info_time_txt">
+                        <p>午前<span>8時</span>から午前<span>11時15分</span></p>
+                    </div>
+                </div>
+                <div class="hero_info_time">
+                    <p>診療開始</p>
+                    <div class="hero_info_time_txt">
+                        <p>午前<span>9時</span>より順次</p>
+                    </div>
+                </div>
+                <div class="hero_info_week">
+                    <div class="on">月</div>
+                    <div class="on">火</div>
+                    <div class="on">水</div>
+                    <div class="on">木</div>
+                    <div class="on">金</div>
+                    <div class="off">土</div>
+                    <div class="off">日</div>
+                    <div class="off">祝</div>
+                </div>
+                <div class="hero_info_tel">
+                    <p>お問い合わせはこちら</p>
+                    <div class="hero_info_tel_num flex_center_column">
+                        <div class="flex">
+
+                            <img src="<?php ?>" alt="">
+                            <p>0577-01-0000</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         <?php else :; ?>
             aa
