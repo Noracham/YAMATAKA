3bb1492b4dd399bba03663557bf857df6f18d324
Nora <nsorym04@gmail.com>
2023-03-07 23:57:58 +0900

    3/7 finish

--- wp-content/themes/YAMATAKA/index.php
+++ wp-content/themes/YAMATAKA/index.php
@@ -58,4 +58,18 @@
         </div>
     </div>
 </section>
+<section class="news wrapper">
+    <div class="news_content">
+        <div class="news_content_new">
+            <div class="news_content_new_inner">
+                <h1>新着情報</h1>
+                <ul class="news_list">
+                    <li><span class="news_list_date">2023/03/03</span><span class="news_list_tag">お知らせ</span>
+                        <p class="news_list_txt">aaaa</p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
 <?php get_footer(); ?>
