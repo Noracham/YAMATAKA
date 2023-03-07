3a21f4e0ccf6090cd806755ae269a4800619bac9
Nora <nsorym04@gmail.com>
2023-03-07 18:39:23 +0900

    heroレスポンシブ

--- wp-content/themes/YAMATAKA/src/js/index.js
+++ wp-content/themes/YAMATAKA/src/js/index.js
@@ -4,10 +4,10 @@ $(function () {
     $header_place = $(window).scrollTop();
 
     if ($header_place > 0) {
-      $(".header").css({ height: "70px", background: "#fff" });
+      $(".header").css({ height: "65px", background: "#fff" });
     } else {
       $(".header").css({
-        height: "75px",
+        height: "70px",
         background: "rgba(255,255,255,0.75)",
       });
     }
