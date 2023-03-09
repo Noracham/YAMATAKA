33dd7ba10b3672b6da9dc7ff4d6f3ec14787d529
Nora <nsorym04@gmail.com>
2023-03-09 12:12:36 +0900

    知る　細かい余白を修正

--- wp-content/themes/YAMATAKA/index.php
+++ wp-content/themes/YAMATAKA/index.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 <section class="know wrapper" id="test">
     <div class="know_content">
-        <div class="know_content_one flex_center_column">
+        <div class="know_content_one flex_column">
             <a href="<?php echo esc_url(home_url()) ?>"></a>
             <div class="know_content_one_title"><img src="<?php echo esc_url(get_theme_file_uri('img/Icon awesome-hospital-alt.svg')); ?>" alt="hospital" />
                 <h2>病院を知る</h2>
@@ -9,14 +9,14 @@
             <p>病院の詳細や案内を確認するにはこちらをご覧ください。<br>
                 所属医師の情報の確認も可能です。</p>
         </div>
-        <div class="know_content_one flex_center_column">
+        <div class="know_content_one flex_column">
             <a href="<?php echo esc_url(home_url()) ?>"></a>
             <div class="know_content_one_title"><img src="<?php echo esc_url(get_theme_file_uri('img/Icon awesome-paper-plane.svg')); ?>" alt="hospitalcross" />
                 <h2>診療科を知る</h2>
             </div>
             <p>当院の緊急休業日や最新の情報など、大切なお知らせなどを確認するにはこちらをご覧ください。</p>
         </div>
-        <div class="know_content_one flex_center_column">
+        <div class="know_content_one flex_column">
             <a href="<?php echo esc_url(home_url()) ?>"></a>
             <div class="know_content_one_title"><img src="<?php echo esc_url(get_theme_file_uri('img/Icon awesome-hospital-alt.svg')); ?>" alt="information" />
                 <h2>情報を知る</h2>
@@ -24,7 +24,7 @@
             <p>病院の詳細や案内を確認するにはこちらをご覧ください。<br>
                 所属医師の情報の確認も可能です。</p>
         </div>
-        <div class="know_content_one flex_center_column">
+        <div class="know_content_one flex_column">
             <a href="<?php echo esc_url(home_url()) ?>"></a>
             <div class="know_content_one_title"><img src="<?php echo esc_url(get_theme_file_uri('img/Icon awesome-map-marker-alt.svg')); ?>" alt="access" />
                 <h2>アクセスを知る</h2>
