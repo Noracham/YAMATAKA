c6a91fbd535601157e8f7cc8242252f016b9890c
Nora <nsorym04@gmail.com>
2023-03-10 12:44:19 +0900

    docter info レスポンシブ

--- wp-content/themes/YAMATAKA/page-hospital.php
+++ wp-content/themes/YAMATAKA/page-hospital.php
@@ -22,31 +22,92 @@
     </div>
 </section>
 <section class="doctor wrapper">
-    <div class="doctor_content inner">
-        <div class="doctor_content_human">
-            <div class="doctor_content_human_img">
-                <img src="<?php echo esc_url(get_theme_file_uri('img/docter.jpg')); ?>" alt="docter">
+    <div class="doctor_inner inner">
+        <div class="doctor_content">
+            <div class="doctor_content_human">
+                <div class="doctor_content_human_img">
+                    <img src="<?php echo esc_url(get_theme_file_uri('img/docter.jpg')); ?>" alt="docter">
+                </div>
+                <p class="doctor_content_name"><span>院長</span>タカヤマヒロシ</p>
             </div>
-            <p class="doctor_content_name"><span>院長</span>タカヤマヒロシ</p>
+            <div class="doctor_content_txt">
+                <div class="doctor_content_txt_one">
+                    <h3>経歴</h3>
+                    <p>
+                        山方片山大学院を卒業後、山方の病院で3年勤務。<br>
+                        その後、小さな病院で副院長を5年務める。<br>
+                        結婚を機に地元にて当院「ヤマカタ病院」を設立。<br>
+                        以後院長として務めている。
+                    </p>
+                </div>
+                <div class="doctor_content_txt_one">
+                    <h3>メッセージ</h3>
+                    <p>
+                        大好きな地元である飛騨高山。<br>
+                        この地で医師として活躍できることに感動と誇りがあります。<br>
+                        皆さんとの繋がりを最も大切としている病院です。<br>
+                        来院の際は気兼ねなく症状などをお伝えください。<br>
+                        全力でご対応いたします。
+                    </p>
+                </div>
+            </div>
         </div>
-        <div class="doctor_content_txt">
-            <div class="doctor_content_txt_one">
-                <h3>経歴</h3>
-                <p>
-                    山方片山大学院を卒業後、山方の病院で3年勤務。<br>
-                    その後、小さな病院で副院長を5年務める。<br>
-                    結婚を機に地元にて当院「ヤマカタ病院」を設立。<br>
-                    以後院長として務めている。
-                </p>
+        <div class="doctor_content">
+            <div class="doctor_content_human">
+                <div class="doctor_content_human_img">
+                    <img src="<?php echo esc_url(get_theme_file_uri('img/docter.jpg')); ?>" alt="docter">
+                </div>
+                <p class="doctor_content_name"><span>院長</span>タカヤマヒロシ</p>
             </div>
-            <div class="doctor_content_txt_one">
-                <h3>経歴</h3>
-                <p>
-                    山方片山大学院を卒業後、山方の病院で3年勤務。<br>
-                    その後、小さな病院で副院長を5年務める。<br>
-                    結婚を機に地元にて当院「ヤマカタ病院」を設立。<br>
-                    以後院長として務めている。
-                </p>
+            <div class="doctor_content_txt">
+                <div class="doctor_content_txt_one">
+                    <h3>経歴</h3>
+                    <p>
+                        山方片山大学院を卒業後、山方の病院で3年勤務。<br>
+                        その後、小さな病院で副院長を5年務める。<br>
+                        結婚を機に地元にて当院「ヤマカタ病院」を設立。<br>
+                        以後院長として務めている。
+                    </p>
+                </div>
+                <div class="doctor_content_txt_one">
+                    <h3>メッセージ</h3>
+                    <p>
+                        大好きな地元である飛騨高山。<br>
+                        この地で医師として活躍できることに感動と誇りがあります。<br>
+                        皆さんとの繋がりを最も大切としている病院です。<br>
+                        来院の際は気兼ねなく症状などをお伝えください。<br>
+                        全力でご対応いたします。
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="doctor_content">
+            <div class="doctor_content_human">
+                <div class="doctor_content_human_img">
+                    <img src="<?php echo esc_url(get_theme_file_uri('img/docter.jpg')); ?>" alt="docter">
+                </div>
+                <p class="doctor_content_name"><span>院長</span>タカヤマヒロシ</p>
+            </div>
+            <div class="doctor_content_txt">
+                <div class="doctor_content_txt_one">
+                    <h3>経歴</h3>
+                    <p>
+                        山方片山大学院を卒業後、山方の病院で3年勤務。<br>
+                        その後、小さな病院で副院長を5年務める。<br>
+                        結婚を機に地元にて当院「ヤマカタ病院」を設立。<br>
+                        以後院長として務めている。
+                    </p>
+                </div>
+                <div class="doctor_content_txt_one">
+                    <h3>メッセージ</h3>
+                    <p>
+                        大好きな地元である飛騨高山。<br>
+                        この地で医師として活躍できることに感動と誇りがあります。<br>
+                        皆さんとの繋がりを最も大切としている病院です。<br>
+                        来院の際は気兼ねなく症状などをお伝えください。<br>
+                        全力でご対応いたします。
+                    </p>
+                </div>
             </div>
         </div>
     </div>
