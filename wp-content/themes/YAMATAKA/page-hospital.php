f0d42abfe26fb98138079c88fa89bb9b45309ff9
Nora <nsorym04@gmail.com>
2023-03-10 12:33:27 +0900

    docter info 土台

--- wp-content/themes/YAMATAKA/page-hospital.php
+++ wp-content/themes/YAMATAKA/page-hospital.php
@@ -1,2 +1,55 @@
 <?php get_header(); ?>
+<section class="hospital_info wrapper">
+    <div class="hospital_info_content">
+        <div class="hospital_info_content_arrow">
+            <div>
+                <h1 class="hospital_info_content_arrow_title">「地域密着」×「距離感」</h1>
+            </div>
+            <div>
+                <p class="hospital_info_content_arrow_subtitle">いつでもそばに</p>
+            </div>
+            <div>
+                <p class="hospital_info_content_arrow_subtitle">いつでもあなたの健康を考えます</p>
+            </div>
+        </div>
+        <div class="circle_wrapper">
+            <div class="hospital_info_content_circle flex_center_column">
+                <h3>みなさまと共に歩む</h3>
+                <p>地域の方々と共に歩んでいく病院。<br>
+                    あなたの健康を真剣に考えます。</p>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="doctor wrapper">
+    <div class="doctor_content inner">
+        <div class="doctor_content_human">
+            <div class="doctor_content_human_img">
+                <img src="<?php echo esc_url(get_theme_file_uri('img/docter.jpg')); ?>" alt="docter">
+            </div>
+            <p class="doctor_content_name"><span>院長</span>タカヤマヒロシ</p>
+        </div>
+        <div class="doctor_content_txt">
+            <div class="doctor_content_txt_one">
+                <h3>経歴</h3>
+                <p>
+                    山方片山大学院を卒業後、山方の病院で3年勤務。<br>
+                    その後、小さな病院で副院長を5年務める。<br>
+                    結婚を機に地元にて当院「ヤマカタ病院」を設立。<br>
+                    以後院長として務めている。
+                </p>
+            </div>
+            <div class="doctor_content_txt_one">
+                <h3>経歴</h3>
+                <p>
+                    山方片山大学院を卒業後、山方の病院で3年勤務。<br>
+                    その後、小さな病院で副院長を5年務める。<br>
+                    結婚を機に地元にて当院「ヤマカタ病院」を設立。<br>
+                    以後院長として務めている。
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+
 <?php get_footer(); ?>
